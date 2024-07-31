14f11aadc7d5a895ec44059a46d23afb488b1929
Jonnygaming Tv <22498524+JonnygamingTv@users.noreply.github.com>
2024-07-31 11:41:22 +0200

    Update Config.cs

--- Config.cs
+++ Config.cs
@@ -5,10 +5,12 @@ namespace Tortellio.BuilderMode
     public class Config : IRocketPluginConfiguration
     {
         public bool EnableServerAnnouncer;
+        public float RestrictiveDistance;
         public string MessageColor;
         public void LoadDefaults()
         {
             EnableServerAnnouncer = true;
+            RestrictiveDistance = 5f;
             MessageColor = "Yellow";
         }
     }
